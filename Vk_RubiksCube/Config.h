96ff1b7cc825cf17f119ab3e01575d847e5cf21d
DESKTOP-8UV8OK0\Sid <sid@abc.com>
2025-04-19 13:53:46 +0530

    Initial Commit and cmakeLists.txt file

---
+++ Vk_RubiksCube/Config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WINDOW_WIDTH  1280  
+#define WINDOW_HEIGHT 720
+
+#define RESOURCE_PATH "C:/Users/Sid/Documents/Visual Studio 2022/Code/Vk_RubiksCube2/Vk_RubiksCube/resources"
+#define SHADER_PATH "C:/Users/Sid/Documents/Visual Studio 2022/Code/Vk_RubiksCube2/Vk_RubiksCube/shaders"
+
+#endif // CONFIG_H
---
+++ Vk_RubiksCube/Vk_RubiksCube.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+#include "VK_RubiksCube.h"
+
+int main()
+{
+    VulkanRenderer renderer;
+
+    try
+    {
+        renderer.run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
