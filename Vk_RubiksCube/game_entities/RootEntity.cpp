3e2ecc15ffccdfb1f6342c3858ad94af86ac9273
Sid <7568554+unrealsid@users.noreply.github.com>
2025-08-17 16:13:48 +0530

    Removed code

--- Vk_RubiksCube/game_entities/RootEntity.cpp
+++ Vk_RubiksCube/game_entities/RootEntity.cpp
@@ -1,13 +1,8 @@
 ﻿#include "RootEntity.h"
 
-float i = 0.0;
-
 void RootEntity::update(double delta_time)
 {
-    //Update data on cpu and write to shared cpu/gpu memory
-    i += 0.05f * delta_time;
-    
-    DrawableEntity::update(delta_time);
+   DrawableEntity::update(delta_time);
 }
 
 void RootEntity::initialize_transform() const
